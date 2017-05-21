575f7bcf63d20002b2f402f67c4ea6a9fe77d395
Ester <Ester ester.ines@gmail.com>
2017-05-21 15:19:42 -0300

    adicionadas dependencias para testes e pagina de teste

---
+++ src/test/java/com/example/Teste.java
@@ -0,0 +1,11 @@
+package com.example;
+import org.fluentlenium.adapter.junit.FluentTest;
+import org.fluentlenium.core.hook.wait.*;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Wait
+public class Teste extends FluentTest{
+
+}
