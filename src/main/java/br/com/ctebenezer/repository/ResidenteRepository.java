beb727a808eae0147ed706dea5481e3c870fa8e0
Aluno <Aluno@200.17.90.220>
2017-06-22 09:21:30 -0300

    alterações para não aparece mais de um residente quando tem mais dependencias

--- src/main/java/br/com/ctebenezer/repository/ResidenteRepository.java
+++ src/main/java/br/com/ctebenezer/repository/ResidenteRepository.java
@@ -11,6 +11,6 @@ import br.com.ctebenezer.domain.Residente;
 @Repository
 public interface ResidenteRepository extends JpaRepository<Residente, Long> {
 	@Query(value="select r.* from CTEBENEZER.RESIDENTE r join CTEBENEZER.ENDERECO e on e.id = r.endereco_id "
-			+ "join CTEBENEZER.RESIDENTE_DEPENDENCIAS rd on rd.residente_id = r.id order by r.nome", nativeQuery=true)
+			+"order by r.nome", nativeQuery=true)
 	public List<Residente> findAll();
 }
