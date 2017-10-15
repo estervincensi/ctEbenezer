4cdd36df01a753803d44a7482f1171203d849db6
Ester <ester.ines@gmail.com>
2017-10-15 19:14:04 -0200

    mais algumas alterações

--- src/main/java/br/com/ctebenezer/controller/RelatorioController.java
+++ src/main/java/br/com/ctebenezer/controller/RelatorioController.java
@@ -37,7 +37,11 @@ public class RelatorioController {
 	@RequestMapping(value = "/chart", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
     String showChart(HttpServletRequest request) {
+		
 		//TODO alterar Json para enviar somente informações importantes para o gráfico
+		//retornar p/grafico1 json: anoX{AltaVoltou:X, DesitenciaVoltou:X}
+		//retornar p/grafico2 json: anoX{NumeroDeAltas:X, NumeroDeDesistencias:X}
+		//retornar p/grafico3 json: anoX{NumeroDeAtendimentos:X}
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json2 = gson.toJson(residenteService.listarTodos());
 
