df480077747721c9ca6a6c62ae7f579a0dd95cae
estervincensi <ester.ines@gmail.com>
2017-11-05 19:17:11 -0200

    corrigida query

--- src/main/java/br/com/ctebenezer/repository/ReceitaRepository.java
+++ src/main/java/br/com/ctebenezer/repository/ReceitaRepository.java
@@ -11,7 +11,7 @@ import br.com.ctebenezer.domain.Receita;
 @Repository
 public interface ReceitaRepository extends JpaRepository<Receita,Long>{
 	
-	@Query(value="Select r.* from receita r join consulta c on r.consulta_id = c.id where r.ativo=true and c.data >= DATEADD(MONTH, -3, GETDATE())",nativeQuery=true)
+	@Query(value="Select r.* from receita r join consulta c on r.consulta_id = c.id where r.ativo=1 and c.data >= DATE_SUB(NOW(), INTERVAL 3 MONTH)",nativeQuery=true)
 	public List<Receita> buscarTodos();
 
 }
